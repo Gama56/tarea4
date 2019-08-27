4d639a0bec3fb234b8e3eeb26a9e516d38c17d11
Gama56 <53376268+Gama56@users.noreply.github.com>
2019-08-27 00:52:20 -0600

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,11 @@
+<?php
+function conectar()
+{
+    $conexion=mysqli_connect("localhost","root","","dbpaginasweb");
+    echo "Funciona!!";
+    if (mysqli_connect_error()) {
+        die('No se puede conectar a la base de datos' . mysqli_connect_error($conexion));
+    }
+    return $conexion;
+}
+?>
